67c4aae24ee91bd0cf2b72957ad4f08e894c9455
0xDRAGOS <sdragos231@gmail.com>
2024-05-04 15:28:52 +0300

    finish footer, header & home;

---
+++ home.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hydra Esports</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include "header.html" ?>
+    
+    <div class="content">
+        <div class="sections">
+            <a href="sections.php">TRAVEL TROUGH 
+                <br>OUR SECTIONS
+            </a>
+        </div>
+        
+        <div class="teams">
+            <a href="teams.php">THE OFFICIAL 
+                <br>HYDRA 
+                <br>TEAMS
+            </a>
+        </div>
+
+        <div class="announcements">
+            <a href="announcements.php">EXPLORE OUR
+                <br>LATEST
+                <br>NEWS
+            </a>
+        </div>
+
+        <div class="gallery">
+            <a href="gallery.php">TAKE AN EYE
+                <br>ON OUR
+                <br>PHOTOS
+            </a>
+        </div>
+        <div class="contact">
+            <a href="contact.php">WANNA GET IN TOUCH
+                <br>WITH US?
+            </a>
+        </div>
+    </div>
+    
+
+    <?php include "footer.html" ?>
+</body>
+</html>
